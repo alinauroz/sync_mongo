4e94670f09b34769f7317882b4f149af2d4e920b
MacBook <alinauroze@hotmail.com>
2020-07-30 14:11:39 +0500

    save added

--- integration/register.js
+++ integration/register.js
@@ -1,8 +1,32 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const dbName = 'Clone';
+const url = "mongodb+srv://admin:abcd1234@cluster0-9zuvz.mongodb.net";
+var connection,db;
 
-MongoClient.connect(url, function(err, client) {
-    const db = client.db(dbName);
-    //client.close();
-  });
+//MongoClient.connect(url, function(err, client) {
+//    const db = client.db(dbName);
+//
+//    collection = db.collection('users');
+//
+//    collection.insert({name: "ali"})
+//});
+
+( async () => {
+    connection = await MongoClient.connect(url);
+    db = await connection.db;
+})()
+
+const save = async data => {
+    try {
+        let collection = db.collection('users');
+        collection.insert(data);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    save
+}
